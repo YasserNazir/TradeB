e209237d238351e4026bdf7cde144e35c2d09466
Yasser <yasserakarimo@gmail.com>
2020-05-16 16:56:56 +0800

    Bakery Shop

---
+++ index.php
@@ -0,0 +1,115 @@
+<?php
+session_start();
+if(isset($_SESSION["uid"])){
+	header("location:profile.php");
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title><link rel="icon" 
+     type="image/png" 
+     href="/Users/yasserkarimo/Downloads/BakeryStore/product_images/Cup_cakes.png"></title>
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+		<style>
+			@media screen and (max-width:480px){
+				#search{width:80%;}
+				#search_btn{width:30%;float:right;margin-top:-32px;margin-right:10px;}
+			}
+		</style>
+	</head>
+<body>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse" aria-expanded="false">
+					<span class="sr-only">navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a href="#" class="navbar-brand"></a>
+			</div>
+		<div class="collapse navbar-collapse" id="collapse">
+			<ul class="nav navbar-nav">
+				<li><a href="index.php"> TradeB</a></li>
+				<li><a href="index.php"><span class="glyphicon glyphicon-modal-window"></span> Product</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				
+				<li><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> SignIn</a>
+					<ul class="dropdown-menu">
+						<div style="width:300px;">
+							<div class="panel panel-primary">
+								<div class="panel-heading">Login</div>
+								<div class="panel-heading">
+									<label for="email">Email</label>
+									<input type="email" class="form-control" id="email" required/>
+									<label for="email">Password</label>
+									<input type="password" class="form-control" id="password" required/>
+									<p><br/></p>
+									<a href="#" style="color:white; list-style:none;"></a><input type="submit" class="btn btn-success" style="float:right;" id="login" value="Login">
+								</div>
+								<div class="panel-footer" id="e_msg"></div>
+							</div>
+						</div>
+					</ul>
+				</li>
+				<li><a href="customer_registration.php"><span class="glyphicon glyphicon-user"></span> SignUp</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+	<p><br/></p>
+	<p><br/></p>
+	<p><br/></p>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-1"></div>
+			<div class="col-md-2 col-xs-12">
+				<div id="get_category">
+				</div>
+				<!--<div class="nav nav-pills nav-stacked">
+					<li class="active"><a href="#"><h4>Categories</h4></a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+				</div> -->
+
+			</div>
+			<div class="col-md-8 col-xs-12">
+				<div class="row">
+					<div class="col-md-12 col-xs-12" id="product_msg">
+					</div>
+				</div>
+				<div class="panel panel-info">
+					<div class="panel-heading">Products</div>
+					<div class="panel-body">
+						<div id="get_product">
+							<!--Here we get product jquery Ajax Request-->
+						</div>
+						<!--<div class="col-md-4">
+							<div class="panel panel-info">
+								<div class="panel-heading">Samsung Galaxy</div>
+								<div class="panel-body">
+									<img src="product_images/images.JPG"/>
+								</div>
+								<div class="panel-heading">$.500.00
+									<button style="float:right;" class="btn btn-danger btn-xs">AddToCart</button>
+								</div>
+							</div>
+						</div> -->
+					</div>
+					<div class="panel-footer">&copy; 2016</div>
+				</div>
+			</div>
+			<div class="col-md-1"></div>
+		</div>
+	</div>
+</body>
+</html>
